648278dbbed0a34c3b6f632292b30377b9063b3e
iammahesh09 <iammahesh.nlr.com>
2020-07-25 12:27:08 +0530

    Global Object

---
+++ README.md
@@ -0,0 +1,5 @@
+##Blog API
+
+#Global Object
+ NodeJs - 'global' is a global object
+ Javascript - 'window' is a global object
--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 let title = "NodeJs";
 console.log(`Welcome to ${title}`);
+
+
+// Global Object
+console.log(global);
+
+// Asynchronous
+setTimeout(() => {
+    console.log(`this is setTimeout function`);
+    clearInterval(setInter);
+}, 3000);
+
+const setInter = setInterval(() => {
+    console.log(`this is setInterval function`)
+}, 1000);
